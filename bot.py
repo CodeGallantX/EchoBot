69e71b9a7c6dae3bb2eb06bbd120798d2675b1e0
John A. Samuel <johnayobami77@proton.me>
2024-12-14 19:02:49 +0100

    Updates

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
 # TOKEN
-print(f"SECRET_TOKEN: {TOKEN}")
 
 # /start command
 async def start(update: Update, context):
@@ -15,11 +14,11 @@ async def echo(update: Update, context):
 
 # help
 async def help(update: Update, context):
-    await.update.message.reply_text("Use `/start` to greet me or send any message to get a reply.")
+    await update.message.reply_text("Use `/start` to greet me or send any message to get a reply.")
 
 # Main function
 def main():
-    application  ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(TOKEN).build()
 
     application.add_handler(CommandHandler('start', start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
